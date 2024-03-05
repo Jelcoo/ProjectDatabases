cbaf5a07fb78fc1a5496a5eac4a5a7b24d8db5f0
Jelco <53396500+Jelcoo@users.noreply.github.com>
2024-03-05 13:39:59 +0100

    Sprint 2 - Variant B (view all activities) (#2)
    * Sprint 2 - Variant A (view all students) (#1)
    
    * Connection Succesful
    
    * Load students on form load
    
    * Added showing student on listitem click, column name phonenumer error typo too
    
    * Fix of phonenumer typo
    
    * Show dashboard on form load
    
    * Better name string
    
    ---------
    
    Co-authored-by: Vabian124 <82999916+Vabian124@users.noreply.github.com>
    
    * Refactor panel show
    
    * Add activities
    
    ---------
    
    Co-authored-by: Vabian124 <82999916+Vabian124@users.noreply.github.com>

---
+++ SomerenModel/Activity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SomerenModel
+{
+    public class Activity
+    {
+        public int ActivityId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime StartOfActivity { get; set; }
+        public DateTime EndOfActivity { get; set; }
+    }
+}
--- SomerenModel/Student.cs
+++ SomerenModel/Student.cs
@@ -4,9 +4,17 @@ namespace SomerenModel
 {
     public class Student
     {
-        public int Id { get; set; }     // database id
-        public string Name { get; set; }
-        public int Number { get; set; } // StudentNumber, e.g. 474791
-        public DateTime BirthDate { get; set; }
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name {
+            get {
+                return FirstName + " " + LastName;
+            }
+        }
+        public string PhoneNumber { get; set; }
+        public string Class { get; set; }
+        public int Vouchers { get; set; }
+        public int RoomId { get; set; }
     }
 }
