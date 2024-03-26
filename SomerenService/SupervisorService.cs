b701a60b9bbbd405ed459c7ae62eb7f19ca751c9
Vabian124 <82999916+Vabian124@users.noreply.github.com>
2024-03-26 11:38:23 +0100

    Service dao en model

---
+++ SomerenService/SupervisorService.cs
@@ -0,0 +1,40 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System.Collections.Generic;
+
+namespace SomerenService
+{
+    public class SupervisorService
+    {
+        private SupervisorDao supervisorDao;
+
+        public SupervisorService()
+        {
+            supervisorDao = new SupervisorDao();
+        }
+
+        // Method to get all supervisors
+        public List<Supervisor> GetSupervisors()
+        {
+            return supervisorDao.GetAll();
+        }
+
+        // Method to get non-participating supervisors
+        public List<Supervisor> GetNonParticipatingSupervisors()
+        {
+            return supervisorDao.GetNonParticipatingSupervisors();
+        }
+
+        // Method to assign a supervisor to a specific activity
+        public void AssignSupervisor(int activityId, int teacherId)
+        {
+            supervisorDao.AssignSupervisor(activityId, teacherId);
+        }
+
+        // Method to unassign a supervisor from an activity
+        public void UnassignSupervisor(int activityId, int teacherId)
+        {
+            supervisorDao.UnassignSupervisor(activityId, teacherId);
+        }
+    }
+}
